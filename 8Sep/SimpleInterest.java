6f66c2d3ecdfc7a76b00ff3199fe56d9befd344c
royston741 <86420701+royston741@users.noreply.github.com>
2022-09-08 17:25:25 +0530

    Update SimpleInterest.java

--- 8Sep/SimpleInterest.java
+++ 8Sep/SimpleInterest.java
@@ -11,17 +11,17 @@ class SimpleInterest
 	public static void main(String args [])
 	{	
 		// principal variable
-		int p=Integer.parseInt(args[0]);
+		int principal=Integer.parseInt(args[0]);
 		// rate of interest variable
-		int r=Integer.parseInt(args[1]);
+		int rateOfInterest=Integer.parseInt(args[1]);
 		// time variable
-		int t=5;
+		int time=5;
 		
 		// Calculating the simple interest	
-		int simpleInterest=p*r*t;
+		int simpleInterest=principal*rateOfInterest*time;
 		
 		// printing the simple interest
-		System.out.println("The simple interest for p = "+p+", r = "+r+", t = "+t+" is : "+simpleInterest);
+		System.out.println("The simple interest for principal = "+principal+", rateofInterest = "+rateOfInterest+", time = "+time+" is : "+simpleInterest);
 	
 	}// main method ends
 	
